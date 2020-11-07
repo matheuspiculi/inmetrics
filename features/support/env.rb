5647116b0b9d599384873fcddcd51e5626c06993
Matheus Piculi <matheus.piculi@avalara.com>
2020-11-07 08:38:26 -0300

    remove sandbox ne2w

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,16 +1,16 @@
-require 'capybara'
-require 'capybara/cucumber'
-require 'capybara/dsl'
-require 'capybara-screenshot/cucumber'
-require 'selenium-webdriver'
-require 'rspec'
-require 'httparty'
-require 'site_prism'
-require 'faker'
-require 'report_builder'
-require 'rubocop'
-require 'base64'
-require 'json-schema'
+require "capybara"
+require "capybara/cucumber"
+require "capybara/dsl"
+require "capybara-screenshot/cucumber"
+require "selenium-webdriver"
+require "rspec"
+require "httparty"
+require "site_prism"
+require "faker"
+require "report_builder"
+require "rubocop"
+require "base64"
+require "json-schema"
 require_relative "commons/dsl.rb"
 
 include Common
@@ -33,21 +33,19 @@ end
 CONFIG = load_config_env
 
 Capybara.register_driver :selenium do |app|
-  if ENV["BROWSER"].eql?('firefox')
+  if ENV["BROWSER"].eql?("firefox")
     Capybara::Selenium::Driver.new(app, browser: :firefox)
-  elsif ENV["BROWSER"].eql?('chrome')
+  elsif ENV["BROWSER"].eql?("chrome")
     Capybara::Selenium::Driver.new(app, browser: :chrome)
-  else ENV["BROWSER"].eql?('chrome_headless')
+  else ENV["BROWSER"].eql?("chrome_headless")
        Capybara::Selenium::Driver.new(app, browser: :chrome,
                                            desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-                                             'chromeOptions' => { 'args' => ['--headless',
-                                                                             '--disable-gpu',
-                                                                             '--disable-dev-shm-usage',
-                                                                             '--disable-extensions',
-                                                                             'disable-infobars',
-                                                                             '--disable-setuid-sandbox',
-                                                                             '--no-sandbox',
-                                                                             '--remote-debugging-port=9222'] }
+                                             "chromeOptions" => { "args" => ["--headless",
+                                                                             "--disable-gpu",
+                                                                             "--disable-dev-shm-usage",
+                                                                             "--disable-extensions",
+                                                                             "disable-infobars",
+                                                                             "--no-sandbox"] }
                                            ))
   end
 end
