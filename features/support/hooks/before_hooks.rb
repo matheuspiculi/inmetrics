9f284a609c27a7e4947e28f25fc1b90734d17e23
Matheus Piculi <matheus.piculi@avalara.com>
2020-11-08 10:47:56 -0300

    Ajustes

--- features/support/hooks/before_hooks.rb
+++ features/support/hooks/before_hooks.rb
@@ -21,33 +21,40 @@ Before('@ui_excluir_funcionario_sucesso') do
   @new_user_api.data_new_user
   @new_user_api.post_data('empregado/cadastrar')
   @new_user_api.record_user_response
-  sleep 5
+  sleep 2
 end
 
 Before('@ui_editar_funcionario_sucesso') do
   @new_user_api.data_new_user
   @new_user_api.post_data('empregado/cadastrar')
   @new_user_api.record_user_response
-  sleep 5
+  sleep 2
 end
 
 Before('@api_alterar_usuario_401') do
   @new_user_api.data_new_user
   @new_user_api.post_data('empregado/cadastrar')
   @new_user_api.record_user_response
-  sleep 5
+  sleep 2
 end
 
 Before('@api_alterar_usuario_404') do
   @new_user_api.data_new_user
   @new_user_api.post_data('empregado/cadastrar')
   @new_user_api.record_user_response
-  sleep 5
+  sleep 2
 end
 
 Before('@api_alterar_usuario_405') do
   @new_user_api.data_new_user
   @new_user_api.post_data('empregado/cadastrar')
   @new_user_api.record_user_response
-  sleep 5
+  sleep 2
+end
+
+Before('@api_alterar_usuario_informacoes_invalidas') do
+  @new_user_api.data_new_user
+  @new_user_api.post_data('empregado/cadastrar')
+  @new_user_api.record_user_response
+  sleep 2
 end
