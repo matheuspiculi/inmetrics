ee5bed624d1cb675e1b225ea0ecbdde28a4048e9
Matheus Piculi <matheus.piculi@avalara.com>
2020-11-08 12:56:07 -0300

    Ajustes

--- features/support/hooks/after_hooks.rb
+++ features/support/hooks/after_hooks.rb
@@ -5,14 +5,14 @@ end
 at_exit do
   time = Time.now
   ReportBuilder.configure do |config|
-      config.json_path = "log/report.json"
-      config.report_path = "log/cucumber_web_report"
-      config.report_types = [:retry, :html]
-      config.color = "yellow"
-      config.report_tabs = %w[Overview Features Scenarios Errors]
-      config.report_title = "InMetrics Report Web Automation Test Results"
-      config.compress_images = false
-      config.additional_info = { "Project name" => "InMetrics", "Browser" => "#{ENV["BROWSER"]}", "Environment" => "Desafio", "Report generated" => time }
+    config.json_path = "log/report.json"
+    config.report_path = "log/cucumber_web_report"
+    config.report_types = [:retry, :html]
+    config.color = "yellow"
+    config.report_tabs = %w[Overview Features Scenarios Errors]
+    config.report_title = "InMetrics Report Web Automation Test Results"
+    config.compress_images = false
+    config.additional_info = { "Project name" => "InMetrics", "Browser" => "#{ENV["BROWSER"]}", "Environment" => "Desafio", "Report generated" => time }
   end
   ReportBuilder.build_report
 end
